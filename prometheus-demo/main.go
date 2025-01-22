fa4612e2e845ab405cfc76fb86f04f8c7447fa6c
Clauvio <2081215119@qq.com>
2025-01-22 10:39:00 +0800

    .

--- prometheus-demo/main.go
+++ prometheus-demo/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -23,6 +24,7 @@ func main() {
 	// 这里注册的容器名称请调用方自行确保存在
 	diskCollector := biz.NewContainerDiskCollector("prometheus")
 	prometheus.MustRegister(diskCollector)
+	prometheus.MustRegister(collectors.NewGoCollector())
 
 	// 对外提供 /metrics 接口，支持 prometheus 采集
 	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
